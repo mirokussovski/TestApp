dc92ea6b6fa0d81cd42eed5a3f79878033549a9e
mirokussovski <miroslav.kussovski@madisoft.com>
2015-07-07 17:07:25 +0300

    new change

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,7 +9,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("change");
+            Console.WriteLine("new change");
         }
     }
 }
